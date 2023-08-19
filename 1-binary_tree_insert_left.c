2056f206b95ab8d2544533680cb136d2175b8d04
LeeWest89 <6683@holbertonstudents.com>
2023-08-19 13:41:58 -0700

    added frees

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,9 +16,15 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	nnode = binary_tree_node(parent, value);
 
 	if (parent == NULL)
+	{
+		free(nnode);
 		return (NULL);
+	}
 	if (nnode == NULL)
+	{
+		free(nnode);
 		return (NULL);
+	}
 
 	if (parent->left != NULL)
 	{
