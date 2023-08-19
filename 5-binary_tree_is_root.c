631aae789f11f1ce42e40746f83a92393d6d9e78
LeeWest89 <6683@holbertonstudents.com>
2023-08-19 14:39:08 -0700

    I think its a root now

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -13,6 +13,8 @@ int binary_tree_is_root(const binary_tree_t *node)
 	if (node == NULL || (node->left == NULL || node->right == NULL))
 		return (0);
 
-	else
+	else if (node != NULL)
 		return (1);
+
+	return (0);
 }
