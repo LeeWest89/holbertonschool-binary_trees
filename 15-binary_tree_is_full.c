9dd9c70e534c0c6538c42e2c73f9251882c345c7
LeeWest89 <6683@holbertonstudents.com>
2023-08-20 17:14:25 -0700

    figure out how to have multiple if statements in one without character count worries

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,6 +1,26 @@
 #include "binary_trees.h"
 
 /**
+ * is_full_of_something - checks if a binary tree is full
+ *
+ * @tree: pointer to the root node of the tree to check
+ * Return: 0 if NULL or tree is full 1 if not 0
+ */
+
+int is_full_of_something(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		if ((tree->left != NULL && tree->right == NULL) ||
+		    (tree->left == NULL && tree->right != NULL) ||
+		    is_full_of_something(tree->left) == 0 ||
+		    is_full_of_something(tree->right) == 0)
+			return (0);
+	}
+	return (1);
+}
+
+/**
  * binary_tree_is_full - checks if a binary tree is full
  *
  * @tree: pointer to the root node of the tree to check
@@ -11,12 +31,5 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-
-	else
-	{
-		if (tree->left == NULL || tree->right == NULL || tree->parent == NULL)
-			return (0);
-		else
-			return (1);
-	}
+	return (is_full_of_something(tree));
 }
